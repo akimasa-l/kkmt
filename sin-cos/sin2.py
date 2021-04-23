ff0f2a0836068fb6ef114a6f3acf52261b47350a
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-04-23 10:57:51 +0900

    Update sin2.py

--- sin-cos/sin2.py
+++ sin-cos/sin2.py
@@ -1,5 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.arange(0,2*np.pi,0.1)
-def f(x):
-    return 
+sep=0.1
+x=np.arange(0,2*np.pi+sep,sep)
+y1=np.sin(x)
+y2=y1**2
+plt.rcParams['text.usetex'] = True
+plt.plot(x,y1,label=r"$\sin x$")
+plt.plot(x,y2,label=r"$\sin^2 x$")
+""" plt.set_xlabel("x")
+plt.set_ylabel("y")
+ """
+plt.grid()
+plt.legend()
+plt.show()
