3712fc2a4458579a11c4908d4b566a866c92a470
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-04-23 11:01:54 +0900

    autopep8ってこういうふうにするんですねぇ〜〜

--- sin-cos/sin2.py
+++ sin-cos/sin2.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
-sep=0.1
-x=np.arange(0,2*np.pi+sep,sep)
-y1=np.sin(x)
-y2=y1**2
+sep = 0.1
+x = np.arange(0, 2 * np.pi + sep, sep)
+y1 = np.sin(x)
+y2 = y1**2
 plt.rcParams['text.usetex'] = True
-plt.plot(x,y1,label=r"$\sin x$")
-plt.plot(x,y2,label=r"$\sin^2 x$")
+plt.plot(x, y1, label=r"$\sin x$")
+plt.plot(x, y2, label=r"$\sin^2 x$")
 """ plt.set_xlabel("x")
 plt.set_ylabel("y")
  """
