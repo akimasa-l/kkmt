ff4218abc0e5a04d14ae662683ebdded23b2dfcd
Liu Mingzheng chromebook <liumingzheng8@outlook.com>
2021-04-23 11:25:18 +0900

    texは諦めました！w

--- sin-cos/cos2.py
+++ sin-cos/cos2.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 sep = 1/16
 x = np.arange(0, 2 * np.pi + sep, sep)
-y1 = np.sin(x)
+y1 = np.cos(x)
 y2 = y1**2
 #plt.rcParams['text.usetex'] = True
-plt.plot(x, y1, label=r"$\sin x$")
-plt.plot(x, y2, label=r"$\sin^2 x$")
+plt.plot(x, y1, label=r"$\cos x$")
+plt.plot(x, y2, label=r"$\cos^2 x$")
 """ plt.set_xlabel("x")
 plt.set_ylabel("y")
  """
